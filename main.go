93075a369e30ba6cd3931417311be69aa2e3bf84
Michael Rapadas <mike@mrap.me>
2014-12-21 04:53:59 -0800

    Ranks wordmap (quicksort). Prints using CLI flags

--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"log"
+)
+
+var (
+	rank      = flag.Bool("rank", false, "Display ranked substrings after digesting words from file.")
+	ranklimit = flag.Int("ranklimit", 0, "Max substrings to print when using -rank. If none, prints all.")
+	filename  = flag.String("filename", "", "Filename of word list.")
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+
+	if *filename == "" {
+		log.Fatal("Requires -filename")
 		return
 	}
 
-	filename := os.Args[1]
-	fmt.Println("Digesting words from", filename)
-	t := CreateWordmap(filename)
+	fmt.Printf("Digesting words from %s...\n", *filename)
+	t := CreateWordmap(*filename)
+	fmt.Printf("Found and indexed %d sub-words (substrings).\n", len(t))
 
-	var (
-		query   string
-		results []string
-	)
+	if *rank {
+		fmt.Println("Ranking words... This could take a while depending on the word list size.")
+		t.PrintRanks(*ranklimit)
+	} else {
+		var (
+			query   string
+			results []string
+		)
 
-	for true {
-		fmt.Print("\nEnter a string to search for: ")
-		fmt.Scanf("%s", &query)
-		results = t.WordsContaining(query)
-		fmt.Printf("===== %d Words Containing '%s' =====\n\n", len(results), query)
-		fmt.Println(results)
+		for true {
+			fmt.Print("\nEnter a string to search for: ")
+			fmt.Scanf("%s", &query)
+			results = t.WordsContaining(query)
+			fmt.Printf("===== %d Words Containing '%s' =====\n\n", len(results), query)
+			fmt.Println(results)
+		}
 	}
+
 }
--- ranker_test.go
+++ ranker_test.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Ranker", func() {
+
 	const testFilename = "test/ranking_test_words.txt"
-	var (
-		_trie *Node
-	)
 
-	BeforeEach(func() {
-		_trie = CreateTrie(testFilename)
-	})
+	Describe("Ranking trie", func() {
+		var (
+			_ranks Ranks
+			_trie  *Node
+		)
 
-	Describe("Ranking most occurent substrings", func() {
-		var _ranks Ranks
 		BeforeEach(func() {
+			_trie = CreateTrie(testFilename)
 			_ranks = RankByOccurences(_trie)
 		})
 
@@ -27,7 +24,24 @@ var _ = Describe("Ranker", func() {
 			Ω(_ranks["th"]).Should(Equal(3))
 			Ω(_ranks["he"]).Should(Equal(3))
 			Ω(_ranks["the"]).Should(Equal(2))
-			fmt.Println(_ranks)
+		})
+	})
+
+	Describe("Ranking a wordmap", func() {
+		var (
+			_ranked []string
+			_wm     Wordmap
+		)
+
+		BeforeEach(func() {
+			_wm = CreateWordmap(testFilename)
+			_ranked = _wm.Ranked()
+		})
+
+		It("should rank", func() {
+			Ω(_ranked[0]).Should(Equal("he"))
+			Ω(_ranked[1]).Should(Equal("th"))
+			Ω(_ranked[2]).Should(Equal("the"))
 		})
 	})
 })
